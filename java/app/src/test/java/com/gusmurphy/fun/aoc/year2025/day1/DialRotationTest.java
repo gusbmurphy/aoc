74795283ebcada9bd99660f7f9fcf217a643824c
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-01 00:18:59 -0600

    Add more tests for `steps()`

--- java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/DialRotationTest.java
+++ java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/DialRotationTest.java
@@ -1,15 +1,26 @@
 package com.gusmurphy.fun.aoc.year2025.day1;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class DialRotationTest {
 
-    @Test
-    void aRotationCanBeBrokenIntoSingleStepIncrements() {
-        var rotation = new DialRotation(DialRotation.Direction.LEFT, 3);
-        Assertions.assertEquals(3, rotation.steps().toList().size());
-        Assertions.assertTrue(rotation.steps().allMatch(step -> step.direction() == DialRotation.Direction.LEFT));
+    @ParameterizedTest
+    @MethodSource("rotations")
+    void aRotationCanBeBrokenIntoSingleStepIncrements(DialRotation rotation) {
+        Assertions.assertEquals(rotation.distance(), rotation.steps().toList().size());
+        Assertions.assertTrue(rotation.steps().allMatch(step -> step.direction() == rotation.direction()));
+    }
+
+    private static Stream<Arguments> rotations() {
+        return Stream.of(
+                Arguments.of(new DialRotation(DialRotation.Direction.LEFT, 8)),
+                Arguments.of(new DialRotation(DialRotation.Direction.RIGHT, 10234))
+        );
     }
 
 }
