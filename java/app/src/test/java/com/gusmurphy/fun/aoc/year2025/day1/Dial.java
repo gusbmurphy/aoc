34856bcc788e6477409213f82847d2d642114adf
Augustus Murphy <gusbmurphy@gmail.com>
2025-11-30 23:44:09 -0600

    Implement rotating to the right

--- java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/Dial.java
+++ java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/Dial.java
@@ -1,7 +1,14 @@
 package com.gusmurphy.fun.aoc.year2025.day1;
 
 public class Dial {
-    public int position() {
-        return 50;
+    private final int position;
+
+    public Dial() { this.position = 50; }
+    private Dial(int position) { this.position = position; }
+
+    public int position() { return position; }
+
+    public Dial rotate(DialRotation rotation) {
+        return new Dial(this.position + rotation.distance());
     }
 }
--- java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/DialTest.java
+++ java/app/src/test/java/com/gusmurphy/fun/aoc/year2025/day1/DialTest.java
@@ -2,6 +2,13 @@ package com.gusmurphy.fun.aoc.year2025.day1;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static com.gusmurphy.fun.aoc.year2025.day1.DialRotation.Direction.*;
 
 public class DialTest {
 
@@ -9,6 +16,20 @@ public class DialTest {
     void aDialStartsAt50() {
         var dial = new Dial();
         Assertions.assertEquals(50, dial.position());
+    }
+
+    @ParameterizedTest
+    @MethodSource("rotationsAndExpectedPositions")
+    void rotatingTheDialReturnsANewOneAtTheNewPosition(DialRotation rotation, int expectedPosition) {
+        var start = new Dial();
+        Dial result = start.rotate(rotation);
+        Assertions.assertEquals(expectedPosition, result.position());
+    }
+
+    private static Stream<Arguments> rotationsAndExpectedPositions() {
+        return Stream.of(
+                Arguments.of(new DialRotation(RIGHT, 5), 55)
+        );
     }
 
 }
