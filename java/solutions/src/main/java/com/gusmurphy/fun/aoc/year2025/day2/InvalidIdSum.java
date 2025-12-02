7366f6004f43a30362d5ced6771fd98bc6fc3d9d
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-02 00:55:14 -0600

    Extract `getSumOfIdsMatchingPredicateInFile`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/InvalidIdSum.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/InvalidIdSum.java
@@ -1,21 +1,23 @@
 package com.gusmurphy.fun.aoc.year2025.day2;
 
+import java.util.function.Predicate;
+
 public class InvalidIdSum {
     private InvalidIdSum() {
     }
 
     public static long forDoubleRepeatIdsInFile(String absolutePath) {
-        return IdRangeListParser.parseFile(absolutePath)
-                .flatMap(ProductIdRange::stream)
-                .filter(ProductId::justTwiceRepeatedSequence)
-                .map(ProductId::value)
-                .reduce(0L, Long::sum);
+        return getSumOfIdsMatchingPredicateInFile(absolutePath, ProductId::justTwiceRepeatedSequence);
     }
 
     public static long forMultipleRepeatIdsInFile(String absolutePath) {
+        return getSumOfIdsMatchingPredicateInFile(absolutePath, ProductId::justRepeatedSequenceAnyNumberOfTimes);
+    }
+
+    private static long getSumOfIdsMatchingPredicateInFile(String absolutePath, Predicate<ProductId> predicate) {
         return IdRangeListParser.parseFile(absolutePath)
                 .flatMap(ProductIdRange::stream)
-                .filter(ProductId::justRepeatedSequenceAnyNumberOfTimes)
+                .filter(predicate)
                 .map(ProductId::value)
                 .reduce(0L, Long::sum);
     }
