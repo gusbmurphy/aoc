ea4557a3e807f90bb672e1ba9021ea5e0e574598
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-01 23:14:30 -0600

    Fully implement `IdRangeListParser`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/IdRangeListParser.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/IdRangeListParser.java
@@ -1,12 +1,19 @@
 package com.gusmurphy.fun.aoc.year2025.day2;
 
 import module java.base;
+import com.gusmurphy.fun.aoc.helper.LineReader;
 
 public class IdRangeListParser {
     private IdRangeListParser() {
     }
 
-    public static List<IdRange> parseFile(String absolutePath) {
-        return new ArrayList<>();
+    public static Stream<IdRange> parseFile(String absolutePath) {
+        return LineReader.readAllLinesFrom(absolutePath)
+                .limit(1)
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .map(string -> {
+                    var split = string.split("-");
+                    return new IdRange(Long.parseLong(split[0]), Long.parseLong(split[1]));
+                });
     }
 }
--- java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day2/IdRangeListParserTest.java
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day2/IdRangeListParserTest.java
@@ -10,8 +10,15 @@ public class IdRangeListParserTest {
     @Test
     void rangesAreParsedFromFile() {
         var file = new File("src/test/resources/year2025/day2/test-id-range.txt");
-        List<IdRange> result = IdRangeListParser.parseFile(file.getAbsolutePath());
-        Assertions.assertNotNull(result);
+        List<IdRange> result = IdRangeListParser.parseFile(file.getAbsolutePath()).toList();
+
+        var expected = List.of(
+                new IdRange(11, 22),
+                new IdRange(95, 115),
+                new IdRange(998, 1012)
+        );
+
+        Assertions.assertIterableEquals(expected, result);
     }
 
 }
