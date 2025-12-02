ea918dc3694b866f1d84184b751cfe9e095fb72c
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-01 23:20:58 -0600

    Create naive `ProductId`

---
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/ProductId.java
@@ -0,0 +1,13 @@
+package com.gusmurphy.fun.aoc.year2025.day2;
+
+public class ProductId {
+    private final long value;
+
+    public ProductId(long id) {
+        this.value = id;
+    }
+
+    public boolean containsTwiceRepeatedSequence() {
+        return true;
+    }
+}
---
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day2/ProductIdTest.java
@@ -0,0 +1,24 @@
+package com.gusmurphy.fun.aoc.year2025.day2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ProductIdTest {
+
+    @ParameterizedTest
+    @MethodSource("idsAndExpectedRepeatResults")
+    void idContainsSequenceRepeatedTwice(ProductId id, Boolean expected) {
+        Assertions.assertEquals(expected, id.containsTwiceRepeatedSequence());
+    }
+
+    private static Stream<Arguments> idsAndExpectedRepeatResults() {
+        return Stream.of(
+                Arguments.of(new ProductId(11), true)
+        );
+    }
+
+}
