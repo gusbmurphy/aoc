83e65654302a104a8dfaf7e3a190b3c65fdd0b9d
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-01 23:47:04 -0600

    Convert `IdRange` to non-record class

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/IdRange.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day2/IdRange.java
@@ -1,4 +1,25 @@
 package com.gusmurphy.fun.aoc.year2025.day2;
 
-public record IdRange(long start, long end) {
+import java.util.Objects;
+
+public class IdRange {
+    private final long start;
+    private final long end;
+
+    public IdRange(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        IdRange idRange = (IdRange) o;
+        return start == idRange.start && end == idRange.end;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
 }
