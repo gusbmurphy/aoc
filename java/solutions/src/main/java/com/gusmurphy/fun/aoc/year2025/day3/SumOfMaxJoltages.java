2b7a870d72f9f6445e57ede0fc26625f512078f4
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-03 11:46:29 -0600

    Add `main` to `SumOfMaxJoltages`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day3/SumOfMaxJoltages.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day3/SumOfMaxJoltages.java
@@ -1,5 +1,10 @@
 package com.gusmurphy.fun.aoc.year2025.day3;
 
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class SumOfMaxJoltages {
     public static long forPairsInFile(String absolutePath) {
         return BatteryBankParser.parseFile(absolutePath)
@@ -15,5 +20,27 @@ public class SumOfMaxJoltages {
                 .map(bank -> bank.maxJoltageOfNBatteries(12))
                 .reduce(Long::sum)
                 .orElseThrow();
+    }
+
+    static void main() {
+        printLineWithDateAndThread("Solving for part 2...");
+        var file = new File("java/solutions/src/main/resources/input/year2025/day3.txt");
+        var result = BatteryBankParser.parseFile(file.getAbsolutePath())
+                .parallel()
+                .map(bank -> {
+                    printLineWithDateAndThread(String.format("Finding max voltage for bank %s...", bank));
+                    var maxVoltage = bank.maxJoltageOfNBatteries(12);
+                    printLineWithDateAndThread(String.format("Max voltage for bank %s: %s", bank, maxVoltage));
+                    return maxVoltage;
+                })
+                .reduce(Long::sum)
+                .orElseThrow();
+        printLineWithDateAndThread(String.format("(%s) Solution for part 2: %s", LocalDateTime.now(), result));
+    }
+
+    private static void printLineWithDateAndThread(String s) {
+        var time = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+        var thread = Thread.currentThread().getName();
+        System.out.printf("%s::%s %s%n", time, thread, s);
     }
 }
