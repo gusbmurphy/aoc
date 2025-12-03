5fc03bb1258efc7c9642de849ec356ca232d5c0d
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-02 23:19:17 -0600

    Parameterize `largestJoltage` test

--- java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day3/BatteryBankTest.java
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day3/BatteryBankTest.java
@@ -1,15 +1,27 @@
 package com.gusmurphy.fun.aoc.year2025.day3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BatteryBankTest {
 
-    @Test
-    void largestJoltage() {
-        var bank = new BatteryBank("11");
-        assertEquals(11, bank.maxJoltageOfTwoBatteries());
+    @ParameterizedTest
+    @MethodSource
+    void largestJoltage(String bankString, int expectedJoltage) {
+        var bank = new BatteryBank(bankString);
+        assertEquals(expectedJoltage, bank.maxJoltageOfTwoBatteries());
+    }
+
+    static Stream<Arguments> largestJoltage() {
+        return Stream.of(
+                Arguments.of("11", 11)
+        );
     }
 
 }
