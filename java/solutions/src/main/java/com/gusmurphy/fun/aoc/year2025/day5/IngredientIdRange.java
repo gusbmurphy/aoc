39b1db2d8966d63e7604c500c4af0f41ff590e1d
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-05 12:42:07 -0600

    Refactor `IngredientIdRange` to use a list of ranges

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day5/IngredientIdRange.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day5/IngredientIdRange.java
@@ -1,63 +1,65 @@
 package com.gusmurphy.fun.aoc.year2025.day5;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class IngredientIdRange {
-    private final long lower;
-    private final long upper;
+    private final List<SingleRange> rangeList;
 
     /**
      * An inclusive range.
      */
     public IngredientIdRange(long lower, long upper) {
-        this.lower = lower;
-        this.upper = upper;
+        rangeList = List.of(new SingleRange(lower, upper));
     }
 
     public LongStream allIds() {
-        return LongStream.rangeClosed(lower, upper);
+        return LongStream.rangeClosed(rangeList.getFirst().lower, rangeList.getFirst().upper);
+    }
+
+    public boolean includes(long id) {
+        return id >= rangeList.getFirst().lower && id <= rangeList.getFirst().upper;
+    }
+
+    public Stream<IngredientIdRange> mergeWith(IngredientIdRange other) {
+        var sorted = Stream.of(this, other).sorted(Comparator.comparingLong(r -> r.rangeList.getFirst().lower)).toList();
+        var startingRange = sorted.getFirst();
+        var endingRange = sorted.getLast();
+
+        if (startingRange.rangeList.getFirst().upper + 1 == endingRange.rangeList.getFirst().lower) {
+            return Stream.of(new IngredientIdRange(startingRange.rangeList.getFirst().lower, endingRange.rangeList.getFirst().upper));
+        }
+
+        if (startingRange.rangeList.getFirst().lower <= endingRange.rangeList.getFirst().lower) {
+            if (endingRange.rangeList.getFirst().lower > startingRange.rangeList.getFirst().upper) {
+                return Stream.of(
+                        startingRange,
+                        endingRange
+                );
+            }
+            if (startingRange.rangeList.getFirst().upper >= endingRange.rangeList.getFirst().upper) {
+                return Stream.of(startingRange);
+            }
+        }
+
+        return Stream.of(new IngredientIdRange(startingRange.rangeList.getFirst().lower, endingRange.rangeList.getFirst().upper));
+    }
+
+    private record SingleRange(long lower, long upper) {
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         IngredientIdRange that = (IngredientIdRange) o;
-        return lower == that.lower && upper == that.upper;
+        return Objects.equals(rangeList, that.rangeList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lower, upper);
-    }
-
-    public boolean includes(long id) {
-        return id >= lower && id <= upper;
-    }
-
-    public Stream<IngredientIdRange> mergeWith(IngredientIdRange other) {
-        var sorted = Stream.of(this, other).sorted(Comparator.comparingLong(r -> r.lower)).toList();
-        var startingRange = sorted.getFirst();
-        var endingRange = sorted.getLast();
-
-        if (startingRange.upper + 1 == endingRange.lower) {
-            return Stream.of(new IngredientIdRange(startingRange.lower, endingRange.upper));
-        }
-
-        if (startingRange.lower <= endingRange.lower) {
-            if (endingRange.lower > startingRange.upper) {
-                return Stream.of(
-                        startingRange,
-                        endingRange
-                );
-            }
-            if (startingRange.upper >= endingRange.upper) {
-                return Stream.of(startingRange);
-            }
-        }
-
-        return Stream.of(new IngredientIdRange(startingRange.lower, endingRange.upper));
+        return Objects.hashCode(rangeList);
     }
 }
