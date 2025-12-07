5feac00ee637833647c8cae0cde6fd86a6a512ec
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-07 11:15:16 -0600

    Add naive `BeamSplitterSolver`

---
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day7/BeamSplitterSolver.java
@@ -0,0 +1,7 @@
+package com.gusmurphy.fun.aoc.year2025.day7;
+
+public class BeamSplitterSolver {
+    public static int countSplitsIn(String fileName) {
+        return 0;
+    }
+}
---
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day7/BeamSplitterSolverTest.java
@@ -0,0 +1,18 @@
+package com.gusmurphy.fun.aoc.year2025.day7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BeamSplitterSolverTest {
+    
+    @Test
+    void noSplitters() {
+        var actual = BeamSplitterSolver.countSplitsIn(pathToExample("no-splits"));
+        Assertions.assertEquals(0, actual);
+    }
+    
+    private static String pathToExample(String exampleName) {
+        return String.format("src/test/resources/year2025/day7/%s.txt", exampleName);
+    }
+    
+}
