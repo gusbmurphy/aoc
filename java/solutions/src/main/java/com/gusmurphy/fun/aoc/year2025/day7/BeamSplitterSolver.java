1cafe9f066e3411f4e3e38c13c621144d3746976
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-08 12:15:31 -0600

    Solve part 2 example with memoized function

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day7/BeamSplitterSolver.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day7/BeamSplitterSolver.java
@@ -3,7 +3,7 @@ package com.gusmurphy.fun.aoc.year2025.day7;
 import com.gusmurphy.fun.aoc.helper.grid.Grid;
 import com.gusmurphy.fun.aoc.helper.grid.GridPosition;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -30,25 +30,42 @@ public class BeamSplitterSolver {
 
     public static int countPathsIn(String fileName) {
         var grid = Grid.fromFile(fileName);
-        var startPosition = grid.positionsOf(START).findFirst().orElseThrow();
-        var splitterPositions = grid.positionsOf(SPLITTER).toList();
-
-        return Stream.iterate(List.of(List.of(startPosition)),
-                        paths -> paths.stream().allMatch(p -> p.stream().allMatch(grid::containsPosition)),
-                        paths -> continuedPathsFrom(paths, splitterPositions))
-                .toList()
-                .getLast()
-                .size();
+        return new PathCounter(grid).countPaths();
     }
 
-    private static List<List<GridPosition>> continuedPathsFrom(List<List<GridPosition>> paths, List<GridPosition> splitterPositions) {
-        List<List<GridPosition>> newPaths = new ArrayList<>();
-        paths.forEach(p -> {
-            var head = p.getLast();
-            var nextPositions = nextPositionsFrom(head, splitterPositions);
-            nextPositions.forEach(next -> newPaths.add(Stream.concat(p.stream(), Stream.of(next)).toList()));
-        });
-        return newPaths;
+    private static class PathCounter {
+        private final HashMap<GridPosition, Integer> memo = new HashMap<>();
+        private final Grid<?> grid;
+        private final List<GridPosition> splitters;
+        private final GridPosition start;
+
+        PathCounter(Grid<Character> grid) {
+            this.start = grid.positionsOf(START).findFirst().orElseThrow();
+            this.grid = grid;
+            splitters = grid.positionsOf(SPLITTER).toList();
+        }
+
+        int countPaths() {
+            return memoizedCountFrom(start);
+        }
+
+        private int memoizedCountFrom(GridPosition p) {
+            if (memo.containsKey(p)) {
+                return memo.get(p);
+            }
+            int result;
+
+            if (!grid.containsPosition(p)) {
+                result = 1;
+            } else if (splitters.contains(p)) {
+                result = memoizedCountFrom(p.toThe(W)) + memoizedCountFrom(p.toThe(E));
+            } else {
+                result = memoizedCountFrom(p.toThe(S));
+            }
+
+            memo.put(p, result);
+            return result;
+        }
     }
 
     private static Stream<GridPosition> nextPositionsFrom(GridPosition p, List<GridPosition> splitterPositions) {
