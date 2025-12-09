a17df170ded00ea2998bcfe364138dcd05cc9872
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-08 20:01:41 -0600

    Add method for distance between two points

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day8/ThreeDimensionalCoordinates.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day8/ThreeDimensionalCoordinates.java
@@ -1,4 +1,11 @@
 package com.gusmurphy.fun.aoc.year2025.day8;
 
 public record ThreeDimensionalCoordinates(int x, int y, int z) {
+    public double distanceTo(ThreeDimensionalCoordinates other) {
+        return Math.sqrt(
+                Math.pow((x - other.x), 2) +
+                Math.pow((y - other.y), 2) +
+                Math.pow((z - other.z), 2)
+        );
+    }
 }
