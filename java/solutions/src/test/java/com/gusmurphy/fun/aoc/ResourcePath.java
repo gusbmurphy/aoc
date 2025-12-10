dd4db834151c26f4c2975721f9365629b012454f
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-09 21:12:12 -0600

    Add `ResourcePath#toRealInput`

--- java/solutions/src/test/java/com/gusmurphy/fun/aoc/ResourcePath.java
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/ResourcePath.java
@@ -7,5 +7,9 @@ public class ResourcePath {
     public static String toTestResource(int day, String name) {
         return String.format("src/test/resources/year2025/day%s/%s.txt", day, name);
     }
+    
+    public static String toRealInput(int day) {
+        return String.format("src/main/resources/input/year2025/day%s.txt", day);
+    }
 
 }
