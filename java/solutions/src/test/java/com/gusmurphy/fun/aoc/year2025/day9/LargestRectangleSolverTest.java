5570176d5d7a5bcdb4d3eae7016304ee1564dd1c
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-14 19:31:19 -0600

    Solve day 9 part 1 real input

--- java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day9/LargestRectangleSolverTest.java
+++ java/solutions/src/test/java/com/gusmurphy/fun/aoc/year2025/day9/LargestRectangleSolverTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LargestRectangleSolverTest {
-    
+
     @Test
     void part1Example() {
         long actual = LargestRectangleSolver.largestRectangleAreaInFile(
@@ -13,5 +13,12 @@ public class LargestRectangleSolverTest {
 
         Assertions.assertEquals(50, actual);
     }
-    
+
+    @Test
+    void part1MyInput() {
+        long actual = LargestRectangleSolver.largestRectangleAreaInFile(
+                ResourcePath.toRealInput(9));
+
+        Assertions.assertEquals(4773451098L, actual);
+    }
 }
