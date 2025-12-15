8a30474be1f56f07e72af178973eb4ce57d53e0e
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-15 16:28:03 -0600

    Add equals and hash code overrides to `Rectangle`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day9/Rectangle.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day9/Rectangle.java
@@ -2,18 +2,37 @@ package com.gusmurphy.fun.aoc.year2025.day9;
 
 import com.gusmurphy.fun.aoc.helper.grid.GridPosition;
 
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 class Rectangle {
     private final GridPosition c1;
     private final GridPosition c2;
     
     public Rectangle(GridPosition c1, GridPosition c2) {
-        this.c1 = c1;
-        this.c2 = c2;
+        var sorted = Stream.of(c1, c2)
+                .sorted(Comparator.comparing(GridPosition::x).thenComparing(GridPosition::y))
+                .toList();
+        this.c1 = sorted.getFirst();
+        this.c2 = sorted.getLast();
     }
     
     public long area() {
         long width = Math.abs(c1.x() - c2.x()) + 1;
         long height = Math.abs(c1.y() - c2.y()) + 1;
         return width * height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Objects.equals(c1, rectangle.c1) && Objects.equals(c2, rectangle.c2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(c1, c2);
     }
 }
