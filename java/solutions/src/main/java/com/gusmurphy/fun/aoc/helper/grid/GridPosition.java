fece4a70366c10b71df636665bef80858398f66f
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-19 14:59:49 -0600

    Add `GridPosition::positionsBetweenExclusive`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/GridPosition.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/GridPosition.java
@@ -1,5 +1,7 @@
 package com.gusmurphy.fun.aoc.helper.grid;
 
+import java.util.stream.Stream;
+
 public record GridPosition(int x, int y) {
     public GridPosition toThe(GridDirection direction) {
         return switch (direction) {
@@ -12,5 +14,12 @@ public record GridPosition(int x, int y) {
             case W -> new GridPosition(this.x - 1, this.y);
             case NW -> new GridPosition(this.x - 1, this.y - 1);
         };
+    }
+
+    public static Stream<GridPosition> positionsBetweenExclusive(GridPosition a, GridPosition b) {
+        var direction = GridDirection.fromAToB(a, b);
+        return Stream.iterate(a, position -> position.toThe(direction))
+                .skip(1)
+                .takeWhile(position -> !position.equals(b));
     }
 }
