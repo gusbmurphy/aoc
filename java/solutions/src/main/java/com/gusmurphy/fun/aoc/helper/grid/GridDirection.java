8dca33e7d245459e9655452e37ab63f36c4ee690
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-19 14:59:35 -0600

    Add `GridDirection::fromAToB`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/GridDirection.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/GridDirection.java
@@ -1,5 +1,47 @@
 package com.gusmurphy.fun.aoc.helper.grid;
 
 public enum GridDirection {
-    N, NE, E, SE, S, SW, W, NW
+    N, NE, E, SE, S, SW, W, NW;
+
+    public static GridDirection fromAToB(GridPosition a, GridPosition b) {
+        if (a.equals(b)) {
+            throw new IllegalArgumentException("Positions must be different");
+        }
+
+        var xDifference = a.x() - b.x();
+        var yDifference = a.y() - b.y();
+
+        if (xDifference == 0) {
+            if (yDifference > 0) {
+                return S;
+            }
+
+            return N;
+        }
+
+        if (yDifference == 0) {
+            if (xDifference > 0) {
+                return E;
+            }
+
+            return W;
+        }
+
+        if (Math.abs(xDifference) != Math.abs(yDifference)) {
+            throw new IllegalArgumentException("Positions must have a straight line of coordinates between them");
+        }
+
+        if (yDifference > 0) {
+            if (xDifference > 0) {
+                return SE;
+            }
+            return SW;
+        }
+
+        if (xDifference > 0) {
+            return NE;
+        }
+
+        return SE;
+    }
 }
