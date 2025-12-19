a5fcd3fde8c539e4d95536ed59e7c3c439a46390
Augustus Murphy <gusbmurphy@gmail.com>
2025-12-19 15:00:18 -0600

    Use `Grid` in `TiledFloor`

--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/Grid.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/helper/grid/Grid.java
@@ -1,5 +1,7 @@
 package com.gusmurphy.fun.aoc.helper.grid;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -7,14 +9,22 @@ public interface Grid<T> {
     static Grid<Character> fromFile(String path) {
         return new FileGrid<>(path, (c) -> c, ' ');
     }
-    
+
     static <T> Grid<T> fromFile(String path, Function<Character, T> characterConverter, T emptyValue) {
         return new FileGrid<>(path, characterConverter, emptyValue);
     }
 
+    static <T> Grid<T> fromPositions(Map<T, Set<GridPosition>> positions, T emptyValue, int width, int height) {
+        return new PositionBasedGrid<>(positions, emptyValue, width, height);
+    }
+
     Stream<Line<T>> rows();
+
     Stream<Line<T>> columns();
+
     boolean containsPosition(GridPosition position);
+
     Stream<GridPosition> positionsOf(T t);
+
     Line<T> lineFromPositionHeading(GridPosition position, GridDirection direction);
 }
--- java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day9/TiledFloor.java
+++ java/solutions/src/main/java/com/gusmurphy/fun/aoc/year2025/day9/TiledFloor.java
@@ -1,22 +1,45 @@
 package com.gusmurphy.fun.aoc.year2025.day9;
 
+import com.gusmurphy.fun.aoc.helper.grid.Grid;
 import com.gusmurphy.fun.aoc.helper.grid.GridPosition;
 
-import java.util.Set;
+import java.util.*;
 
 public class TiledFloor {
+    private final Grid<Tile> grid;
     private final Set<GridPosition> redTilePositions;
 
-    private TiledFloor(Set<GridPosition> redTilePositions) {
-        this.redTilePositions = redTilePositions;
+    private TiledFloor(List<GridPosition> redTilePositionList) {
+        var greenPositions = new HashSet<GridPosition>();
+
+        for (int i = 0, j = 1; j < redTilePositionList.size(); i++, j++) {
+            var lineStart = redTilePositionList.get(i);
+            var lineEnd = redTilePositionList.get(j);
+            GridPosition.positionsBetweenExclusive(lineStart, lineEnd)
+                    .forEach(greenPositions::add);
+        }
+
+        var redTilePositionSet = new HashSet<>(redTilePositionList);
+        this.redTilePositions = redTilePositionSet;
+        Map<Tile, Set<GridPosition>> tileColorPositions = Map.of(
+                Tile.RED, redTilePositionSet, Tile.GREEN, greenPositions);
+
+        var width = redTilePositionList.stream().mapToInt(GridPosition::x).max().orElseThrow() + 2;
+        var height = redTilePositionList.stream().mapToInt(GridPosition::y).max().orElseThrow() + 2;
+
+        grid = Grid.fromPositions(tileColorPositions, Tile.BLANK, width, height);
     }
 
-    public static TiledFloor withRedTilesAtPositions(Set<GridPosition> positions) {
+    public static TiledFloor withRedTilesAtPositions(List<GridPosition> positions) {
         return new TiledFloor(positions);
     }
 
 
     public boolean positionIsWithinTiles(GridPosition position) {
         return redTilePositions.contains(position);
+    }
+
+    private enum Tile {
+        RED, GREEN, BLANK
     }
 }
